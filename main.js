09db7ad55d43ec6bc80d9430f37b4c29348e79fd
HuggyDaBuggy <hugo@maxtedfamily.net>
2021-02-24 20:43:34 +0800

    the thingos

---
+++ main.js
@@ -0,0 +1,35 @@
+var roleHarvester = require("role.harvester");
+var roleUpgrader = require("role.upgrader");
+var roleBuilder = require("role.builder");
+var harvesters = 0;
+var upgraders = 0;
+var builders = 0;
+var spawnRandom;
+module.exports.loop = function () {
+  for (var name in Game.rooms) {
+    console.log('Room "' + name + '" has ' + Game.rooms[name].energyAvailable + " energy");
+  }
+  for (var name in Game.creeps) {
+    var creep = Game.creeps[name];
+    if (creep.memory.role == "harvester") {
+      roleHarvester.run(creep);
+    } else if (creep.memory.role == "upgrader") {
+      roleUpgrader.run(creep);
+    } else if (creep.memory.role == "builder") {
+      roleBuilder.run(creep);
+    }
+  }
+  if (Game.spawns["Spawn1"].energy >= 150) {
+    spawnRandom = Math.floor(Math.random() * 3);
+    if (spawnRandom == 0) {
+      Game.spawns["Spawn1"].spawnCreep([WORK, CARRY, MOVE], "Upgrader" + upgraders, {memory: {role: "upgrader"}});
+      upgraders++;
+    } else if (spawnRandom == 1) {
+      Game.spawns["Spawn1"].spawnCreep([WORK, CARRY, MOVE], "Harvester" + harvesters, {memory: {role: "harvester"}});
+      harvesters++;
+    } else {
+      Game.spawns["Spawn1"].spawnCreep([WORK, CARRY, MOVE], "Builder" + builders, {memory: {role: "builder"}});
+      builders++;
+    }
+  }
+};
