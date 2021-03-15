41ec01860ff561ab94e547e9f8a58311127275e2
HuggyDaBuggy <hugo@maxtedfamily.net>
2021-03-15 08:09:12 +0800

    thing

--- main.js
+++ main.js
@@ -6,9 +6,6 @@ var upgraders = 0;
 var builders = 0;
 var spawnRandom;
 module.exports.loop = function () {
-  for (var name in Game.rooms) {
-    console.log('Room "' + name + '" has ' + Game.rooms[name].energyAvailable + " energy");
-  }
   for (var name in Game.creeps) {
     var creep = Game.creeps[name];
     if (creep.memory.role == "harvester") {
